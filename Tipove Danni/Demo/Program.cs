fa914110741af4bd993f1fc41fef7455eb2a5f30
kristiyan-arnaudov05 <krisitiyan_arnaudov@abv.bg>
2022-01-29 11:55:07 +0200

    sdasda

---
+++ Tipove Danni/Demo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte age = 12; //-128 to 127
+            byte b = 23; //0 to 255
+            short s = 4; //[-32 768 … 32 767]
+            ushort us = 5; //[0 … 65 535]
+            int a = 1000; //[-2 147 483 648 … 2 147 483 647]
+            uint ui = 12; //[0 … 4 294 967 295]
+            long l = 34; //[-9 223 372 036 854 775 808 … 9 223 372 036 854 775 807]
+            ulong ul = 322;//[0 … 18 446 744 073 709 551 615]
+
+        }
+    }
+}
